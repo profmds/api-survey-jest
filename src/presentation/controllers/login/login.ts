5b9ffcb9a13ae06d62b548929425fdb996ee8025
Marcelo dos Santos <profmds@gmail.com>
2021-03-23 21:28:11 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,3 +1,4 @@
+import { Authentication } from '../../../domain/usecases/authentication'
 import { InvalidParamError, MissingParamError } from '../../error'
 import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
@@ -5,31 +6,37 @@ import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
-  constructor (emailValidator: EmailValidator) {
+  private readonly authentication: Authentication
+
+  constructor (emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator
+    this.authentication = authentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      let errorRequest
       const { email, password } = httpRequest.body
 
       if (!email) {
-        errorRequest = new MissingParamError('email')
+        return await new Promise(resolve => resolve(
+          badRequest(new MissingParamError('email'))
+        ))
       }
       if (!password) {
-        errorRequest = new MissingParamError('password')
+        return await new Promise(resolve => resolve(
+          badRequest(new MissingParamError('password'))
+        ))
       }
 
       const isValid = this.emailValidator.isValid(email)
 
       if (!isValid) {
-        errorRequest = new InvalidParamError('email')
+        return await new Promise(resolve => resolve(
+          badRequest(new InvalidParamError('email'))
+        ))
       }
 
-      return await new Promise(resolve => resolve(
-        badRequest(errorRequest)
-      ))
+      await this.authentication.auth(email, password)
     } catch (error) {
       return serverError(error)
     }
