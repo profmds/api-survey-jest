3f75ed784c10f56ac5145556ecf05703a6d0b3f5
Marcelo dos Santos <profmds@gmail.com>
2021-03-25 14:41:04 -0300

    refactor: move sut creation to a fatory method

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,15 +1,19 @@
 import { MissingParamError } from '../../errors'
 import { RequeiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequeiredFieldValidation => {
+  return new RequeiredFieldValidation('field')
+}
+
 describe('RequeiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequeiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('Should not return if validation succeeds', () => {
-    const sut = new RequeiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_name' })
     expect(error).toBeFalsy()
   })
