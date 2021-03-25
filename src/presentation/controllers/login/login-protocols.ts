1bcc3257f46183c83b658832c1ac68fcaad6542b
Marcelo dos Santos <profmds@gmail.com>
2021-03-25 15:54:41 -0300

    feat: add LoginValidationFactory

--- src/presentation/controllers/login/login-protocols.ts
+++ src/presentation/controllers/login/login-protocols.ts
@@ -1,4 +1,5 @@
 export * from '../../protocols'
-export * from '../../protocols/email-validator'
 
 export * from '../../../domain/usecases/authentication'
+
+export * from '../../helpers/validators/validation'
