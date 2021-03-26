20e30c9a74a9addf334ea27384289442b5c5deea
Marcelo dos Santos <profmds@gmail.com>
2021-03-26 14:04:40 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
-  port: process.env.PORT || 3333
+  port: process.env.PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || 'aa00faf97d042c13a59da4d27eb32358'
 }
