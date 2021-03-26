fbd01b35ee876f42bf9c2f411a1ba203a6133e05
Marcelo dos Santos <profmds@gmail.com>
2021-03-26 14:13:01 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await mongoHelper.getCollection('accounts')
+    accountCollection = await mongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,24 @@ describe('Login Routes', () => {
           email: 'profmds@gmail.com',
           password: 'meupassword',
           passwordConfirmation: 'meupassword'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const passwordHashed = await hash('meupassword', 12)
+      await accountCollection.insertOne({
+        name: 'Marcelo',
+        email: 'profmds@gmail.com',
+        password: passwordHashed
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'profmds@gmail.com',
+          password: 'meupassword'
         })
         .expect(200)
     })
