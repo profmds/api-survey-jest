fb353b1dabacef4b184093182d97475db0959055
Marcelo dos Santos <profmds@gmail.com>
2021-03-26 14:16:01 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 with invalid credentials on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'profmds@gmail.com',
+          password: 'meupassword'
+        })
+        .expect(401)
+    })
   })
 })
